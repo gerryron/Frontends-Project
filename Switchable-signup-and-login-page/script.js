01fb21c026472e676a620129cf8f4bb90a1c7646
Gerryron <gerryron02@gmail.com>
2022-05-04 10:29:26 +0700

    add swichable-signup-and-login-page

---
+++ Switchable-signup-and-login-page/script.js
@@ -0,0 +1,23 @@
+/* function to open form based on formName */
+function openForm(evt, formName) {
+  // Declare all variables
+  var i, switchcontent, switchlinks;
+
+  // Get all elements with class="switchcontent" and hide them using for loop
+  switchcontent = document.getElementsByClassName('switchcontent');
+  for (i = 0; i < switchcontent.length; i++) {
+    switchcontent[i].style.display = 'none';
+  }
+
+  // Get all button elements with class="switchlink" and remove the class "active" for loop
+  switchlinks = document.getElementsByClassName('switchlink');
+  for (i = 0; i < switchlinks.length; i++) {
+    switchlinks[i].className = switchlinks[i].className.replace(' active', '');
+  }
+
+  document.getElementById(formName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+// show default form
+document.getElementById('defaultOpen').click();
